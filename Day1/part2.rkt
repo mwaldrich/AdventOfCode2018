8ab98fef96ce58d05de8fe8f8a68017c55a4da7b
Mark Aldrich <mastermark51200@gmail.com>
2018-12-01 20:18:28 -0500

    refactored day 1 part 2

--- Day1/part2.rkt
+++ Day1/part2.rkt
@@ -1,25 +1,16 @@
 #lang racket
 
-; calculate-frequencies : (Listof Number) Number -> (Listof Number)
-; Calculates the resulting frequencies after applying the given deltas to the
-; given start frequency. The most recent frequency appears first in the
-; resulting list.
-(define (calculate-frequencies deltas start)
-  (define (helper deltas current previous)
-    (let ([new (cons current previous)])
-      (if (empty? deltas)
-        new
-        (helper (rest deltas)
-                (+ (first deltas) current)
-                new))))
-  (helper deltas start '()))
-
-(define (find-first-change-repeat deltas)
-  (define (helper previous)
-    (let ([first-dup (check-duplicates (reverse previous))])
-      (or first-dup
-          (helper (append (calculate-frequencies deltas (first previous))
-                          (rest previous))))))
+;; Find the first frequency repeated when repeatedly applying the given deltas.
+(define (find-first-frequency-repeat deltas)
+  ;; newest frequencies stored first
+  (define (helper frequencies)
+    ;; are there any duplicates in our list of accumulated frequencies?
+    (or (check-duplicates (reverse frequencies))
+        ;; apply the given deltas to our frequencies
+        (helper (foldl (λ (delta deltas)
+                                 (cons (+ delta (first deltas)) deltas))
+                               frequencies
+                               deltas))))
   (helper '(0)))
 
-(find-first-change-repeat (file->list "input.txt"))
+(find-first-frequency-repeat (file->list "input.txt"))
